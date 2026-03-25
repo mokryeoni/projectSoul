bec52ab6a077e0125ee5a092285d09606a797756
starryhj <2507110310x@gmail.com>
2026-03-25 16:41:20 +0900

    feat: 스프링 부트 프로젝트 초기 설정

---
+++ mycar/mycar/src/test/java/com/soul/mycar/MycarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soul.mycar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
